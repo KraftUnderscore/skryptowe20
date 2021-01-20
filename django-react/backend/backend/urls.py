c0b7782085e3059549a3a7de2486d89984709bee
spieta <spieta2@gmail.com>
2021-01-20 20:04:28 +0100

    Backup

--- django-react/backend/backend/urls.py
+++ django-react/backend/backend/urls.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
 from exchange import views
-
-router = routers.DefaultRouter()
-router.register(r'rates', views.CurrencyRatesView, 'rates')
-router.register(r'summary', views.CurrencySummaryView, 'summary')
-router.register(r'orders', views.OrdersView, 'orders') # TODO delete after testing
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    path('api/rates/', views.CurrencyRatesView.as_view(), name="rates"),
+    path('api/summary/', views.CurrencySummaryView.as_view(), name="summary"),
 ]
--- django-react/backend/exchange/views.py
+++ django-react/backend/exchange/views.py
@@ -1,22 +1,19 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.generics import ListAPIView
 from .serializers import \
     CurrencyRatesSerializer, \
-    CurrencySummarySerializer, \
-    OrdersSerializer
-from .models import CurrencyRates, CurrencySummary, Orders
+    CurrencySummarySerializer
+from .models import CurrencyRates, CurrencySummary
 
-# TODO The Views allow for CRUD operations on data, make it GET only.
 # TODO might help with retriving correct objects https://docs.djangoproject.com/en/3.1/topics/db/queries/#retrieving-specific-objects-with-filters
 
-class CurrencyRatesView(viewsets.ModelViewSet):
+class CurrencyRatesView(ListAPIView):
   serializer_class = CurrencyRatesSerializer
   queryset = CurrencyRates.objects.all()
+  filter_backends = [DjangoFilterBackend]
+  filterset_fields = ['currency', 'date']
 
-class CurrencySummaryView(viewsets.ModelViewSet):
+class CurrencySummaryView(ListAPIView):
   serializer_class = CurrencySummarySerializer
   queryset = CurrencySummary.objects.all()
-
-class OrdersView(viewsets.ModelViewSet):
-  serializer_class = OrdersSerializer
-  queryset = Orders.objects.all()
